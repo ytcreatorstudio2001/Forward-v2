d100f4171685ba9722cd1799d2e04bb09bfad8c9
Ytstudio <91581305+ytcreatorstudio2001@users.noreply.github.com>
2022-02-02 17:06:21 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -84,9 +84,9 @@ async def pub_(bot, message):
                 await m.edit_text(f'Error: {e}')
             else:
                 buttons = [[
-                    InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk')
+                    InlineKeyboardButton('📜 Support Group', url='https://t.me/MovieHub_2k22')
                     ],[
-                    InlineKeyboardButton('📡 Update Channel', url='https://t.me/DX_Botz')
+                    InlineKeyboardButton('📡 Update Channel', url='https://t.me/MovieHub_2k22')
                 ]]
                 reply_markup = InlineKeyboardMarkup(buttons)
                 await m.edit_text(
